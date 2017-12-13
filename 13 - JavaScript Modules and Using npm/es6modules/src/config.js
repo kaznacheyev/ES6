665e30e37df7444230152a78c52a93a2e72b60ac
Yaroslav Kaznacheyev <kaznacheyev88@gmail.com>
2017-12-13 23:34:32 +0200

    modules

--- 13 - JavaScript Modules and Using npm/es6modules/src/config.js
+++ 13 - JavaScript Modules and Using npm/es6modules/src/config.js
@@ -4,7 +4,7 @@ export const apiKey = 'abc123';
 export const url = 'http://wesbos.com';
 
 export function sayHi(name) {
-  console.log(`Hello there ${name}`);
+	console.log(`Hello there ${name}`);
 }
 
 const age = 100;
--- 13 - JavaScript Modules and Using npm/es6modules/src/user.js
+++ 13 - JavaScript Modules and Using npm/es6modules/src/user.js
@@ -3,15 +3,15 @@ import { url } from './config';
 import md5 from 'md5';
 
 export default function User(name, email, website) {
-  return { name, email, website };
+	return { name, email, website };
 }
 
 export function createURL(name) {
-  return `${url}/users/${slug(name)}`;
+	return `${url}/users/${slug(name)}`;
 }
 
 export function gravatar(email) {
-  const hash = md5(email.toLowerCase());
-  const photoURL = `https://www.gravatar.com/avatar/${hash}`;
-  return photoURL;
+	const hash = md5(email.toLowerCase());
+	const photoURL = `https://www.gravatar.com/avatar/${hash}`;
+	return photoURL;
 }
